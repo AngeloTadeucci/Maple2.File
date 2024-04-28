0fcad42ac696edb715e365038d805a5184a01f17
Zin <62830952+Zintixx@users.noreply.github.com>
2024-04-27 17:37:39 -0700

    Parse NpcScript Condition and Function

---
+++ Maple2.File.Parser/ServerTableParser.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Xml;
+using System.Xml.Serialization;
+using Maple2.File.IO;
+using Maple2.File.Parser.Xml.Table.Server;
+
+namespace Maple2.File.Parser;
+
+public class ServerTableParser {
+    private readonly M2dReader xmlReader;
+    private readonly XmlSerializer npcScriptConditionSerializer;
+    private readonly XmlSerializer npcScriptFunctionSerializer;
+
+    public ServerTableParser(M2dReader xmlReader) {
+        this.xmlReader = xmlReader;
+        this.npcScriptConditionSerializer = new XmlSerializer(typeof(NpcScriptConditionRoot));
+        this.npcScriptFunctionSerializer = new XmlSerializer(typeof(NpcScriptFunctionRoot));
+
+        // var seen = new HashSet<string>();
+        // this.bankTypeSerializer.UnknownAttribute += (sender, args) => {
+        //     if (!seen.Contains(args.Attr.Name)) {
+        //         Console.WriteLine($"Missing {args.Attr.Name}={args.Attr.Value}");
+        //         seen.Add(args.Attr.Name);
+        //     }
+        // };
+    }
+
+    public IEnumerable<(int NpcId, int ScriptID, NpcScriptCondition ScriptCondition)> ParseNpcScriptCondition() {
+        XmlReader reader = xmlReader.GetXmlReader(xmlReader.GetEntry("table/Server/npcScriptCondition_Final.xml"));
+        var data = npcScriptConditionSerializer.Deserialize(reader) as NpcScriptConditionRoot;
+        Debug.Assert(data != null);
+
+        foreach (NpcScriptCondition scriptCondition in data.condition) {
+            yield return (scriptCondition.npcID, scriptCondition.scriptID, scriptCondition);
+        }
+    }
+    
+    public IEnumerable<(int NpcId, int ScriptId, NpcScriptFunction ScriptFunction)> ParseNpcScriptFunction() {
+        XmlReader reader = xmlReader.GetXmlReader(xmlReader.GetEntry("table/Server/npcScriptFunction_Final.xml"));
+        var data = npcScriptFunctionSerializer.Deserialize(reader) as NpcScriptFunctionRoot;
+        Debug.Assert(data != null);
+
+        foreach (NpcScriptFunction scriptFunction in data.function) {
+            yield return (scriptFunction.npcID, scriptFunction.scriptID, scriptFunction);
+        }
+    }
+}
---
+++ Maple2.File.Tests/ServerTableParserTest.cs
@@ -0,0 +1,25 @@
+﻿using Maple2.File.Parser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Maple2.File.Tests;
+
+[TestClass]
+public class ServerTableParserTest {
+    [TestMethod]
+    public void TestNpcScriptCondition() {
+        var parser = new ServerTableParser(TestUtils.ServerReader);
+
+        foreach ((_, _, _) in parser.ParseNpcScriptCondition()) {
+            continue;
+        }
+    }
+    
+    [TestMethod]
+    public void TestNpcScriptFunction() {
+        var parser = new ServerTableParser(TestUtils.ServerReader);
+
+        foreach ((int _, _, _) in parser.ParseNpcScriptFunction()) {
+            continue;
+        }
+    }
+}
