a5e32690c9ba18c136eb706c2dec8fe3d1e01de7
Zin <62830952+Zintixx@users.noreply.github.com>
2024-04-28 01:47:51 -0700

    script, shop, and bonusgame tables

---
+++ Maple2.File.Parser/Xml/Table/Server/ShopBeauty.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using M2dXmlGenerator;
+using Maple2.File.Parser.Enum;
+
+namespace Maple2.File.Parser.Xml.Table.Server;
+
+// ./data/server/table/Server/NA/shop_beauty.xml
+[XmlRoot("ms2")]
+public partial class ShopBeautyRoot {
+    [XmlElement] public List<ShopBeauty> shop;
+}
+
+public partial class ShopBeauty {
+    [XmlAttribute] public int shopID;
+    [XmlAttribute] public int categoryID;
+    [M2dEnum] public PaymentType colorPaymentType;
+    [XmlAttribute] public int colorPrice;
+    [M2dEnum] public PaymentType stylePaymentType;
+    [XmlAttribute] public int stylePrice;
+    [XmlAttribute] public string couponTag;
+    [XmlAttribute] public int displayCouponID;
+    [XmlAttribute] public int returnCouponID;
+    [XmlAttribute] public bool random;
+    [M2dFeatureLocale(Selector = "id")] private IList<ShopBeauty.Item> _item;
+    [XmlElement] public List<ItemGroup> itemGroup;
+    
+    public partial class Item : IFeatureLocale {
+        [XmlAttribute] public int id;
+        [M2dEnum] public PaymentType paymentType;
+        [XmlAttribute] public int price;
+        [XmlAttribute] public int weight;
+        [XmlAttribute] public int achieveID;
+        [XmlAttribute] public short achieveGrade;
+        [XmlAttribute] public short requireLevel;
+        [M2dEnum] public ShopItemFrameType saleTag;
+    }
+
+    public partial class ItemGroup {
+        [XmlAttribute] public int probVer;
+        [XmlAttribute] public string saleStartTime;
+        [M2dFeatureLocale(Selector = "id")] private IList<ShopBeauty.Item> _item;
+    }
+}
---
+++ Maple2.File.Parser/Xml/Table/Server/ShopGame.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+using M2dXmlGenerator;
+using Maple2.File.Parser.Enum;
+
+namespace Maple2.File.Parser.Xml.Table.Server;
+
+// ./data/server/table/Server/shop_game.xml
+[XmlRoot("ms2")]
+public partial class ShopGameRoot {
+    [XmlElement] public List<ShopGame> shop;
+}
+
+public partial class ShopGame {
+    [XmlAttribute] public int shopID;
+    [M2dFeatureLocale(Selector = "sn")] private IList<ShopGame.Item> _item;
+
+    public partial class Item : IFeatureLocale {
+        [XmlAttribute] public int sn;
+        [XmlAttribute] public int id;
+        [XmlAttribute] public short grade;
+        [M2dEnum] public PaymentType paymentType;
+        [XmlAttribute] public bool premiumItem;
+        [XmlAttribute] public int paymentItemID;
+        [XmlAttribute] public string paymentIconTag;
+        [XmlAttribute] public long price;
+        [XmlAttribute] public long prob;
+        [XmlAttribute] public bool randomOption;
+        [XmlAttribute] public string category;
+        [XmlAttribute] public bool wearForPreview;
+        [XmlAttribute] public int sellCount;
+        [XmlAttribute] public int sellUnit;
+        [XmlAttribute] public string startDate;
+        [XmlAttribute] public string endDate;
+        [M2dEnum] public ShopItemFrameType frameType;
+        [XmlAttribute] public byte saleTag;
+        [XmlAttribute] public string requireAchieve;
+        [XmlAttribute] public string requireAlliance;
+        [XmlAttribute] public byte requireAllianceGrade;
+        [M2dArray] public int[] requireChampionshipInfo;
+        [M2dArray] public string[] requireGuildNpc;
+    }
+}
