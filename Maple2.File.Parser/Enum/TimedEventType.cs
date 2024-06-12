9bc680ec360e12014d40b6ae479f50aab14d0eb3
Zin <62830952+Zintixx@users.noreply.github.com>
2024-06-11 23:45:43 -0700

    Parse TimeEventData

---
+++ Maple2.File.Parser/Enum/TimedEventType.cs
@@ -0,0 +1,11 @@
+﻿namespace Maple2.File.Parser.Enum;
+
+public enum TimedEventType {
+    Boss = 1,
+    HiddenRealmPortal = 2,
+    Unknown3 = 3,
+    FieldEvent = 4, // balloons, etc.
+    GlobalEvent = 5,
+    KritiasChestPuzzle = 6,
+    KritiasInvasion = 7,
+}
