9d2291660f1bf06374319cde0d4cb48597df9663
Ângelo Tadeucci <angelo_tadeucci@hotmail.com.br>
2025-02-08 21:17:34 -0300

    Parse Ugc Housing Point Reward

---
+++ Maple2.File.Parser/Xml/Table/UgcHousingPointReward.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace Maple2.File.Parser.Xml.Table;
+
+// ./data/xml/table/ugchousingpointreward.xml
+[XmlRoot("ms2")]
+public partial class UgcHousingPointRewardRoot {
+    [XmlElement("v")] public List<UgcHousingPointReward> Entries;
+}
+
+public partial class UgcHousingPointReward {
+    [XmlAttribute] public int housingPoint;
+    [XmlAttribute] public int individualDropBoxId;
+}
